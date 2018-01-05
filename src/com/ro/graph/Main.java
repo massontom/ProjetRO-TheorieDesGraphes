bcd3e0eea8596c1a4e27dc374694cfc2d0865963
Thomas Boucher <thomas.boucher@insa-rouen.fr>
2018-01-05 14:38:28 +0100

    modif choix menu

--- src/com/ro/graph/Main.java
+++ src/com/ro/graph/Main.java
@@ -7,72 +7,57 @@ import org.graphstream.graph.implementations.*;
 public class Main {
   public static void main(String[] args) throws Exception {
     UniteTemporelle uTemp = UniteTemporelle.MOIS;
-    UniteSpatiale uSpat = UniteSpatiale.COMMUNES;
+    UniteSpatiale uSpat = UniteSpatiale.DEPARTEMENTS;
     GestionBD bd = new GestionBD();
     Scanner sc = new Scanner(System.in); //test menus
-    boolean arret = false;
+    int choix;
+
+    do {
+      System.out.println("---Choix de l'unité spatiale ---");
+      System.out.println("Par communes : tapez 1");
+      System.out.println("Par départements : tapez 2");
+      System.out.println("quitter : tapez 9");
+      choix = sc.nextInt();
+      switch (choix) {
+      case 1:
+        uSpat = UniteSpatiale.COMMUNES;
+        break;
+      case 2:
+        uSpat = UniteSpatiale.DEPARTEMENTS;
+        break;
+      }
+    } while (choix != 9);
+
+    choix = 0;
+
+    do {
+      System.out.println("---Choix de l'unité temporelle---");
+      System.out.println("Par mois : tapez 1");
+      System.out.println("Par jour : tapez 2");
+      System.out.println("Par heure : tapez 3");
+      System.out.println("Par minute : tapez 4");
+      System.out.println("quitter : tapez 9");
+      choix = sc.nextInt();
+      switch (choix) {
+      case 1:
+        uTemp = UniteTemporelle.MOIS;
+        break;
+      case 2:
+        uTemp = UniteTemporelle.JOUR;
+        break;
+      case 3:
+        uTemp = UniteTemporelle.HEURE;
+        break;
+      case 4:
+        uTemp = UniteTemporelle.MINUTE;
+        break;
+      }
+    } while (choix != 9);
+
+    System.out.println(String.format("Vos choix sont : %s, %s", uTemp, uSpat));
+
     CustomGraph customGraph = new CustomGraph(bd, uTemp, uSpat);
-
-  bd.deconnexion();
-
-
-  while (!arret){
-	System.out.println("Par communes : tapez 1");
-	System.out.println("Par départements : tapez 2");
-  System.out.println("quitter : tapez 9");
-  int choix= sc.nextInt();
-
-	switch(choix)
-	{
-		case 1 :{
-					int secondChoice = 0;
-					System.out.println("Par mois : tapez 1");
-					System.out.println("Par jour : tapez 2");
-					System.out.println("Par heure : tapez 3");
-          System.out.println("Par minute : tapez 4");
-          System.out.println("quitter : tapez 9");
-					secondChoice = sc.nextInt();
-					switch(secondChoice){
-						case 1 : {bd.genererRequete(UniteTemporelle.MOIS);
-                      System.out.println(bd.genererRequete(UniteTemporelle.MOIS));
-                      System.out.println("Par communes et mois");
-                      } break; //ajoutez les requetes correspondantes
-						case 2 :{bd.genererRequete(UniteTemporelle.JOUR);
-                      System.out.println(bd.genererRequete(UniteTemporelle.JOUR));
-                      System.out.println("Par communes et jour");
-                    } break;
-            case 3 : {bd.genererRequete(UniteTemporelle.HEURE);
-                      System.out.println(bd.genererRequete(UniteTemporelle.HEURE));
-                      System.out.println("Par communes et heure");
-                      } break;
-            case 4 : {bd.genererRequete(UniteTemporelle.MINUTE);
-                      System.out.println(bd.genererRequete(UniteTemporelle.MINUTE));
-                      System.out.println("Par communes et minute");
-                      } break;
-            case 9 : arret = true; break;
-            default : System.out.println("entrez un choix entre 1 et 4"); break;
-					}
-				} break;
-		case 2 :{
-        System.out.println("Par mois : tapez 1");
-        System.out.println("Par jour : tapez 2");
-        System.out.println("Par heure : tapez 3");
-        System.out.println("Par minute : tapez 4");
-        System.out.println("quitter : tapez 9");
-        int thirdChoice = sc.nextInt();
-        switch(thirdChoice){
-            case 1 : System.out.println("Par département et mois"); break; //requetes à ajouter ici aussi
-            case 2 : System.out.println("Par département et jour"); break;
-            case 3 : System.out.println("Par departement et heure"); break;
-            case 4 : System.out.println("Par département et minute"); break;
-            case 9 : arret = true; break;
-            default : System.out.println("entrez un choix entre 1 et 4"); break;
-          }
-        } break;
-    case 9 : arret = true; break;
-		default : System.out.println("entrez un choix entre 1 et 3"); break;
-	}
-                }
-  customGraph.afficher();
+    customGraph.afficher();
+    bd.deconnexion();
   }
 }
