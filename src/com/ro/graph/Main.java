85442218cdd361a50e141d8f5ed7bafa23dd9eb2
Simon <simon.brunet@insa-rouen.fr>
2018-01-05 11:24:41 +0100

    Modifs menu

--- src/com/ro/graph/Main.java
+++ src/com/ro/graph/Main.java
@@ -33,10 +33,22 @@ public class Main {
           System.out.println("quitter : tapez 9");
 					secondChoice = sc.nextInt();
 					switch(secondChoice){
-						case 1 : System.out.println("Par communes et mois"); break; //ajoutez les requetes correspondantes
-						case 2 : System.out.println("Par communes et jour"); break;
-            case 3 : System.out.println("Par communes et heure"); break;
-            case 4 : System.out.println("Par communes et minute"); break;
+						case 1 : {bd.genererRequete(UniteTemporelle.MOIS);
+                      System.out.println(bd.genererRequete(UniteTemporelle.MOIS));
+                      System.out.println("Par communes et mois");
+                      } break; //ajoutez les requetes correspondantes
+						case 2 :{bd.genererRequete(UniteTemporelle.JOUR);
+                      System.out.println(bd.genererRequete(UniteTemporelle.JOUR));
+                      System.out.println("Par communes et jour");
+                    } break;
+            case 3 : {bd.genererRequete(UniteTemporelle.HEURE);
+                      System.out.println(bd.genererRequete(UniteTemporelle.HEURE));
+                      System.out.println("Par communes et heure");
+                      } break;
+            case 4 : {bd.genererRequete(UniteTemporelle.MINUTE);
+                      System.out.println(bd.genererRequete(UniteTemporelle.MINUTE));
+                      System.out.println("Par communes et minute");
+                      } break;
             case 9 : arret = true; break;
             default : System.out.println("entrez un choix entre 1 et 4"); break;
 					}
@@ -49,10 +61,10 @@ public class Main {
         System.out.println("quitter : tapez 9");
         int thirdChoice = sc.nextInt();
         switch(thirdChoice){
-            case 1 : System.out.println("Par communes et mois"); break; //requetes à ajouter ici aussi
-            case 2 : System.out.println("Par communes et jour"); break;
-            case 3 : System.out.println("Par communes et heure"); break;
-            case 4 : System.out.println("Par communes et minute"); break;
+            case 1 : System.out.println("Par département et mois"); break; //requetes à ajouter ici aussi
+            case 2 : System.out.println("Par département et jour"); break;
+            case 3 : System.out.println("Par departement et heure"); break;
+            case 4 : System.out.println("Par département et minute"); break;
             case 9 : arret = true; break;
             default : System.out.println("entrez un choix entre 1 et 4"); break;
           }
