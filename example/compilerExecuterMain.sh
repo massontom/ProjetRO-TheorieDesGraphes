621c407ad98675436c765887f23b8781c0cc93ee
Unknown <thomas.boucher@insa-rouen.fr>
2018-01-06 13:23:21 +0100

    compile script example

--- example/compilerExecuterMain.sh
+++ example/compilerExecuterMain.sh
@@ -1,4 +1,6 @@
 rm -rf classes/*.class*
-javac -encoding UTF8 -sourcepath sources -classpath librairies/postgresql-42.1.4.jar:librairies/gs-ui-1.3/gs-ui-1.3.jar:librairies/gs-core-1.3/gs-core-1.3.jar:librairies/gs-algo-1.3/gs-algo-1.3.jar -d classes sources/unite/*.java
-javac -encoding UTF8 -sourcepath sources -classpath librairies/postgresql-42.1.4.jar:librairies/gs-ui-1.3/gs-ui-1.3.jar:librairies/gs-core-1.3/gs-core-1.3.jar:librairies/gs-algo-1.3/gs-algo-1.3.jar -d classes sources/*.java
-java -classpath  librairies/postgresql-42.1.4.jar:librairies/gs-ui-1.3/gs-ui-1.3.jar:librairies/gs-core-1.3/gs-core-1.3.jar:librairies/gs-algo-1.3/gs-algo-1.3.jar:classes/ Main
+javac -encoding UTF8 -sourcepath sources -classpath ./../gs/postgresql-42.1.4.jar:./../gs/gs-ui-1.3.jar:./../gs/gs-core-1.3.jar:./../gs/gs-algo-1.3.jar -d classes sources/unite/*.java
+
+javac -encoding UTF8 -sourcepath sources -classpath ./../gs/postgresql-42.1.4.jar:./../gs/gs-ui-1.3.jar:./../gs/gs-core-1.3.jar:./../gs/gs-algo-1.3.jar -d classes sources/*.java
+
+java -classpath  ./../gs/postgresql-42.1.4.jar:./../gs/gs-ui-1.3.jar:./../gs/gs-core-1.3.jar:./../gs/gs-algo-1.3.jar:classes/ Main
