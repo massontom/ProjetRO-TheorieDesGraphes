dc1d74f68f6753f7887e5d4739648ecaebb8f5c2
Tom Masson <tom.masson@insa-rouen.fr>
2018-01-07 21:51:44 +0100

    clean git

--- example/compilerExecuterMain.sh
+++
@@ -1,6 +0,0 @@
-rm -rf classes/*.class*
-javac -encoding UTF8 -sourcepath sources -classpath ./../gs/postgresql-42.1.4.jar:./../gs/gs-ui-1.3.jar:./../gs/gs-core-1.3.jar:./../gs/gs-algo-1.3.jar -d classes sources/unite/*.java
-
-javac -encoding UTF8 -sourcepath sources -classpath ./../gs/postgresql-42.1.4.jar:./../gs/gs-ui-1.3.jar:./../gs/gs-core-1.3.jar:./../gs/gs-algo-1.3.jar -d classes sources/*.java
-
-java -classpath  ./../gs/postgresql-42.1.4.jar:./../gs/gs-ui-1.3.jar:./../gs/gs-core-1.3.jar:./../gs/gs-algo-1.3.jar:classes/ Main
